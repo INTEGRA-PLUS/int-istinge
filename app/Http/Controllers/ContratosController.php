519f6f34eba3adc7065d1f05a11cd6a264074429
higuita1498 <higuitajokers@gmail.com>
2025-09-23 15:32:28 -0500

    fix:corrgiendo mensaje de consultas mk

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -1829,6 +1829,8 @@ class ContratosController extends Controller
                 }
 
                 $API->disconnect();
+            }else{
+                $registro = true;
             }
 
                 if ($registro) {
