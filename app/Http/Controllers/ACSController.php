cb5a65a820a6922cd37f9ff51f2dca8318a9ed1b
Ronal David Valderrama Gomez <189985514+ronaldavidvalderrama@users.noreply.github.com>
2025-09-26 11:51:33 -0500

    feat: :sparkles: rutas adecuadas

---
+++ app/Http/Controllers/ACSController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ACSController extends Controller
+{
+    public function index()
+    {
+        return view('acs.index', [
+            'title' => 'ACS'
+        ]);
+    }
+}
