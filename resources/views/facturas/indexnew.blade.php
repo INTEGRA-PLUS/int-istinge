64c11e1249087072c06dc63eba9261b1f504cb63
Uriel Vargas <fabianvargas1099@gmail.com>
2025-11-21 09:01:06 -0500

    fix: :bug: Intesogamoso export facturas

--- resources/views/facturas/indexnew.blade.php
+++ resources/views/facturas/indexnew.blade.php
@@ -1049,9 +1049,26 @@
 	}
 
 	function exportar() {
-		$("#estado").selectpicker('refresh');
-        window.location.href = window.location.pathname+'/exportar?codigo='+$('#codigo').val()+'&cliente='+$('#cliente').val()+'&municipio='+$('#municipio').val()+'&barrio='+$('#barrio').val()+'&creacion='+$('#creacion').val()+'&vencimiento='+$('#vencimiento').val()+'&estado='+$('#estado').val()+'&grupos_corte='+$('#grupos_corte').val()+'&fact_siigo='+$('#fact_siigo').val()+'&state_contrato='+$('#state_contrato').val()+'&tipo=1';
-	}
+        $("#estado").selectpicker('refresh');
+
+        let baseUrl = "{{ route('facturas.exportar') }}";
+
+        let params = [
+            'codigo=' + encodeURIComponent($('#codigo').val() || ''),
+            'cliente=' + encodeURIComponent($('#cliente').val() || ''),
+            'municipio=' + encodeURIComponent($('#municipio').val() || ''),
+            'barrio=' + encodeURIComponent($('#barrio').val() || ''),
+            'creacion=' + encodeURIComponent($('#creacion').val() || ''),
+            'vencimiento=' + encodeURIComponent($('#vencimiento').val() || ''),
+            'estado=' + encodeURIComponent($('#estado').val() || ''),
+            'grupos_corte=' + encodeURIComponent($('#grupos_corte').val() || ''),
+            'fact_siigo=' + encodeURIComponent($('#fact_siigo').val() || ''),
+            'state_contrato=' + encodeURIComponent($('#state_contrato').val() || ''),
+            'tipo=1'
+        ].join('&');
+
+        window.location.href = baseUrl + '?' + params;
+    }
 
 	@if($tipo)
 	    $('#estado').val('{{ $tipo }}').selectpicker('refresh');
