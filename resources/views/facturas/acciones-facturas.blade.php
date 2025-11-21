fc6f7997a2b6b6dd7d9421aebd8e542c246cf182
higuita1498 <higuitajokers@gmail.com>
2025-11-21 15:43:14 -0500

    fix: improve readability of payment promise condition in acciones-facturas.blade.php

--- resources/views/facturas/acciones-facturas.blade.php
+++ resources/views/facturas/acciones-facturas.blade.php
@@ -17,8 +17,14 @@
         <a href="{{route('facturas.edit',$factura->id)}}"  class="btn btn-outline-primary btn-icons" title="Editar"><i class="fas fa-edit"></i></a>
         @endif
         @if(isset($_SESSION['permisos']['775']))
-        @if(isset($factura->promesa_pago) && $factura->promesa_pago==null ||isset($factura->promesa_pago) && $factura->$promesa_pago < Carbon\Carbon::now()->format('Y-m-d'))
-        <a href="javascript:modificarPromesa('{{$factura->id}}')" class="btn btn-outline-danger btn-icons promesa" idfactura="{{$factura->id}}" title="Promesa de Pago"><i class="fas fa-calendar"></i></a>
+        @if(
+            (isset($factura->promesa_pago) && $factura->promesa_pago == null)
+            ||
+            (isset($factura->promesa_pago) && $factura->promesa_pago < \Carbon\Carbon::now()->format('Y-m-d'))
+        )
+            <a href="javascript:modificarPromesa('{{$factura->id}}')" class="btn btn-outline-danger btn-icons promesa" idfactura="{{$factura->id}}" title="Promesa de Pago">
+                <i class="fas fa-calendar"></i>
+            </a>
         @endif
         @endif
 	@endif
