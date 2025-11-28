095bae4b1db8212f953bf78ed8b4b4882b17af7f
higuita1498 <higuitajokers@gmail.com>
2025-11-28 11:58:53 -0500

    fix: add nro_factura_vencida field to grupos_corte update logic for improved data handling

--- resources/views/facturas/acciones-facturas.blade.php
+++ resources/views/facturas/acciones-facturas.blade.php
@@ -18,6 +18,7 @@
         @endif
         @if(isset($_SESSION['permisos']['775']))
         @if(
+
             ($factura->promesa_pago == null) || ($factura->promesa_pago < \Carbon\Carbon::now()->format('Y-m-d'))
         )
             <a href="javascript:modificarPromesa('{{$factura->id}}')" class="btn btn-outline-danger btn-icons promesa" idfactura="{{$factura->id}}" title="Promesa de Pago">
