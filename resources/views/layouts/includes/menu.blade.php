191547f034b885adfa1f0944cbae0c5135a2353e
Uriel Vargas <fabianvargas1099@gmail.com>
2025-12-04 12:25:05 -0500

    fix: :sparkles: chat meta solo para los que tienen instancias meta

--- resources/views/layouts/includes/menu.blade.php
+++ resources/views/layouts/includes/menu.blade.php
@@ -335,9 +335,11 @@ isset($_SESSION['permisos']['804']) || isset($_SESSION['permisos']['858'])
                 <a class="nav-link" href="{{ route('crm.chat_IA') }}">Chat IA</a>
             </li>
             @endif
+            @if(Auth::user()->empresa()->wppNuevo == 1)
             <li class="nav-item" id="crm_chat_meta">
                 <a class="nav-link" href="{{ route('crm.chat_meta') }}">Chat Meta</a>
             </li>
+            @endif
             {{-- @if(Auth::user()->empresa()->chat_ia == 1) --}}
             <li class="nav-item" id="crm_whatsapp">
                 <a class="nav-link" href="{{route('crm.chatboxIA')}}">Chatbox</a>
