fd15543c3a537090c0912c25f4ca25d767eae46b
Uriel Vargas <fabianvargas1099@gmail.com>
2025-12-20 09:59:21 -0500

    fix: .

--- resources/views/avisos/envio.blade.php
+++ resources/views/avisos/envio.blade.php
@@ -224,13 +224,14 @@
 	const plantillasMeta = isFiberNet ? plantillasMetaFiberNet : plantillasMetaDefault;
 
 	const mensajesPlantillasMeta = {
-		suspension: "Buenos Dias, INTEGRA te informa que estas programado para corte de tu servicio de internet y tv el día de hoy, Para evitar la suspensión del servicio efectúe el pago",
-		corte: "Buenos Dias, INTEGRA te informa que estas programado para corte de tu servicio de internet y tv el día de mañana, Para evitar la suspensión del servicio efectúe el pago",
-		recordatorio: "Buenos Dias, INTEGRA le recuerda que para restablecer el servicio, recuerda efectuar el pago en nuestra oficina o consignando a la cuenta, si lo haces por este ultimo medio recuerda enviar el comprobante. Muchas Gracias.",
-		factura: "Estimado cliente Juan Perez. INTEGRA le informa que se ha generado su factura por valor de 40.000 $ puedes realizar el pago a través del siguiente link."
-		falla_servicio: "Se presenta una falla general en el servicio. Nuestro equipo técnico se encuentra trabajando para restablecerlo lo antes posible. Agradecemos su comprensión.",
-    	dano_fibra_principal: "Cordial Saludo, Informamos a nuestros usuarios del sector de Tocogua que se está presentado un daño en la fibra principal, parte técnica ya está en el sector realizando los arreglos pertinentes, en el trascurso de la tarde se reestablece el servicio, Agradecemos su comprensión."
-	};
+        suspension: "Buenos Dias, INTEGRA te informa que estas programado para corte de tu servicio de internet y tv el día de hoy, Para evitar la suspensión del servicio efectúe el pago",
+        corte: "Buenos Dias, INTEGRA te informa que estas programado para corte de tu servicio de internet y tv el día de mañana, Para evitar la suspensión del servicio efectúe el pago",
+        recordatorio: "Buenos Dias, INTEGRA le recuerda que para restablecer el servicio, recuerda efectuar el pago en nuestra oficina o consignando a la cuenta, si lo haces por este ultimo medio recuerda enviar el comprobante. Muchas Gracias.",
+        factura: "Estimado cliente Juan Perez. INTEGRA le informa que se ha generado su factura por valor de 40.000 $ puedes realizar el pago a través del siguiente link.",
+        falla_servicio_: "Se presenta una falla general en el servicio. Nuestro equipo técnico se encuentra trabajando para restablecerlo lo antes posible. Agradecemos su comprensión.",
+        dao_de_fibra_principal: "Cordial saludo. Informamos a nuestros usuarios que se está presentando un daño en la fibra principal. El equipo técnico ya se encuentra atendiendo la novedad. Agradecemos su comprensión."
+    };
+
 
 	// ============================================================
 	// GUARDAR PLANTILLAS NORMALES (desde el backend)
